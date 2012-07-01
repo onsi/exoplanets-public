59a6e9a00e92303c9cdb3f4a3610bc147a143abd
Onsi Fakhouri <onsijoe@gmail.com>
2012-07-01 16:36:54 -0700

    added preview for new theme

---
+++ preview/exoFrontend.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+    })();
+});
