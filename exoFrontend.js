1380138c32276ba0444a1ba745cc73dfd0340cce
Onsi Fakhouri <onsijoe@gmail.com>
2012-10-28 21:31:10 -0700

    added planet counts

--- exoFrontend.js
+++ exoFrontend.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    $.getJSON('/columns/counts', function(counts) {
+      $('.count.exoplanets').html(counts['exoplanets']);
+      $('.count.imaged').html(counts['imaged']);
+      $('.count.total').html(counts['total']);
+      $('.count.kepler').html(counts['kepler']);
+      $('.count.total_with_kepler').html(counts['total_with_kepler']);
+      $('.count.loading').removeClass('loading');
+    });
+
     (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
